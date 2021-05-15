5266cf636dae73d8bc41194bb201594e2a1d2405
Berat Ünver <unverberat@gmail.com>
2021-05-15 17:04:11 +0300

    Final version added via desktop

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function compute()
-{
+function compute(){
+
+    //Get the values and calculate
     p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    //Check for positive values
+    var principal = document.getElementById("principal").value;
+    var check = parseInt(principal) > 0;
+    if (!check) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return
+    }
+    //Create the Interest text
+    document.getElementById("result").innerHTML = "If you deposit <mark>"+ principal +"</mark>,<br/>" +
+        "at an interest rate of <mark>"+ rate +"%</mark>.<br/>" +
+        "You will receieve an amount of <mark>"+ interest +"</mark>,<br/>" +
+        "in the year <mark>"+ year +"</mark>.<br/><br/>";
     
 }
-        
+
+function updateRate(){
+    //Update the rate of slider
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML = rateval + "%";
+}
